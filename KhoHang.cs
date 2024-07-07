d9f4d1e8813b949b900d3fd3e912dc19bd072cf7
tranvantin-T4 <tranvantin2009.2@gmail.com>
2024-07-07 07:23:12 +0700

    Add files via upload

---
+++ KhoHang.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using DevExpress.Persistent.Base;
+namespace DANHOM.Module.QLCuaHangPB
+{
+    [DefaultClassOptions]
+    public partial class KhoHang
+    {
+        public KhoHang(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
